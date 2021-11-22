beeb6fcb9176e7324ae09966226e0ba03958c3fa
a Milán <hbazsi024@gmail.com>
2021-11-22 20:59:00 +0100

    Testing cases #1

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,8 +1,25 @@
 #include "print.h"
+#include "../../intf/time.h"
+
+void ct(){
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+    char* t = asctime(timeinfo);
+    print_str(t);
+}
+
+
+void print_title(){
+    print_clear();
+    print_set_color(PRINT_COLOR_MAGENTA, PRINT_COLOR_BLACK);
+    print_str("2048-OS - @tcgmilan");
+}
 
 void kernel_main(){
-    print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("2048-OS - @tcgmilan");
-
+    print_title();
+    ct();
 }
+
